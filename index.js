b3b6ac9c11af543319055726b8e5c9e98b1bed57
kashish00208 <141568392+kashish00208@users.noreply.github.com>
2024-08-07 17:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-require('dotenv').config() 
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/login', (req, res) => {
-    res.send('<h1>Successful login detected</h1>')
+const users = require("./DATA.json");
+
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+  res.end("Restfull API");
+});
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users", (req, res) => {
+  const data = `<ul>${users
+    .map((user) => `<li>${user.first_name}<li/>`)
+    .join("")}<ul/>`;
+  return res.send(data);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => id === user.id);
+  return res.send(user);
+});
+
+app.listen(port, () => {
+  console.log("port rendering");
+});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => id === user.id);
+    return res.send(user);
   })
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  .put((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "pending" });
+  });
+
+app.post('/api/users',(req,res)=>{
+    const body = req.body;
+    return res.json({ status: "pending" });
+    console.log(body)
 })
